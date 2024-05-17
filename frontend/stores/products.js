c36a4625a6104e8fd258f5cf221ed6b8a57fb086
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-05-17 12:00:16 -0600

    installed nanostores for global state

---
+++ frontend/stores/products.js
@@ -0,0 +1,12 @@
+export const $Products = atom([
+  {
+    id: 1,
+    name: "Product 1",
+    price: 100,
+    description: "Description 1",
+    image: "image1.jpg",
+    category: "category1",
+    createdAt: "2021-01-01",
+    updatedAt: "2021-01-01",
+  },
+]);
