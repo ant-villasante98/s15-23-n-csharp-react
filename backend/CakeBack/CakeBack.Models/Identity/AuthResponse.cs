86f585e6d0327c327f5d49f4e9155f9701b361b1
Deivison Jimenez <deivi.jimenez@gmail.com>
2024-05-22 21:45:55 -0400

    Se crea Arquitectura Indentity, primera Interfaz Generica de CRUD

---
+++ backend/CakeBack/CakeBack.Models/Identity/AuthResponse.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace CakeBack.Models.Identity
+{
+    public class AuthResponse
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }= string.Empty;
+
+        public string Email {  get; set; }= string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+    }
+}
---
+++ backend/CakeBack/CakeBack.Models/Identity/RegistrationRequest.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace CakeBack.Models.Identity
+{
+    public class RegistrationRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
