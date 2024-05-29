e08301f9b210dbd799abad00c5269ef7b67ad2f4
Johanna Hernandez <johannaviglianco@gmail.com>
2024-05-29 00:27:27 -0300

    Agrego dependencia axios y pagina para mostar el cart.

---
+++ frontend/app/cart/page.jsx
@@ -0,0 +1,16 @@
+import Cart from "@/components/Cart";
+
+
+export const metadata = {
+  title: "Shopping-Cart Baking Love",
+  description:
+    "Shopping-Cart-Baking Love",
+};
+
+export default function ShoppingCart() {
+  return (
+    <main className="">
+     <Cart />
+    </main>
+  );
+}
