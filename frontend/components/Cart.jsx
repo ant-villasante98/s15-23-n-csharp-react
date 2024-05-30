95f90971be31c8fd994b5db4384929bfb8b2eed9
Johanna Hernandez <johannaviglianco@gmail.com>
2024-05-29 22:33:48 -0300

    Estilos

--- frontend/components/Cart.jsx
+++ frontend/components/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("https://s4kn44kn-9080.brs.devtunnels.ms/api/v1/shopping-carts");
+        const response = await axios.get("https://s4kn44kn-9080.brs.devtunnels.ms/api/v1/shopping-carts"); /*/products.json*/ 
         const data = response.data;
         setProducts(data);
 
@@ -80,74 +80,74 @@ const Cart = () => {
   return (
     <>
       <div className="sm:w-full mx-auto bg-gray-50 rounded-md">
-        <h3 className="flex-1 justify-center content-center h-20 bg-gray-100 text-pink-400 text-center text-xl font-semibold border-b-2 border-slate-100 shadow-sm mb-3">
+        <h3 className="flex-1 justify-center content-center h-20 bg-gray-100 text-pink-400 text-center text-xl font-semibold border-b-2 border-slate-100 shadow-sm ">
           Cart:{" "}
         </h3>
       </div>
 
-      <ul className="sm:grid sm:grid-cols-1 p-6 lg:m-10">
+      <ul className="sm:pl-0 sm:pr-0 bg-purple-200 sm:grid sm:grid-cols-1 p-6 lg:p-10">
         {products.map((product) => (
           <li
             key={product.productId}
-            className="sm:h-64 content-between border-b border-gray-200 sm:grid sm:grid-cols-4 sm:grid-rows-5 md:auto-rows-auto sm:gap-x-4 sm:justify-items-center sm:mb-8 md:justify-between lg:justify-around md:grid-cols-5 md:grid-rows-3 md:h-auto md:pt-4 md:pb-4 md:gap-x-4 md:items-center md:content-center lg:grid lg:grid-cols-5 lg:items-center"
+            className=" items-center bg-pink-100 rounded-md mt-2 sm:h-64 content-between shadow-lg  border-pink-200 sm:grid sm:grid-cols-4 sm:grid-rows-5 md:auto-rows-auto sm:gap-x-6 sm:justify-items-center sm:mb-8 md:justify-between lg:justify-around md:grid-cols-5 md:grid-rows-3 md:h-auto md:pt-4 md:pb-4 md:gap-x-4 md:items-center md:content-center lg:grid lg:grid-cols-5 lg:items-center  "
           >
             <img
               src={product.image}
               alt={product.productName}
-              className="sm:w-12 sm:h-12 sm:col-start-1 sm:row-start-3 md:col-start-1 md:row-start-3 lg:w-20 lg:h-20 lg:row-start-3"
+              className="sm:text-sm md:text-md sm:w-12 sm:h-12 sm:col-start-1 sm:row-start-3 md:col-start-1 md:row-start-3 lg:w-20 lg:h-20 lg:row-start-3"
             />
             <div className="sm:row-start-1 sm:col-start-1">
-              <p className="text-md font-bold text-pink-500 lg:text-lg">
+              <p className="sm:text-sm md:text-md  font-bold text-pink-500 lg:text-lg">
                 Producto:
               </p>
             </div>
             <div className="name/price sm:col-start-1 sm:row-start-2 md:col-start-1 md:row-start-2">
-              <h4 className="text-base text-start pb-1 font-bold text-pink-400 lg:text-lg">
+              <h4 className=" sm:text-sm md:text-md text-base text-start pb-1 font-bold text-pink-400 lg:text-lg">
                 {product.productName}
               </h4>
             </div>
             <div className="sm:row-start-2 sm:col-start-2 md:col-start-2 md:row-start-2">
-              <p className="text-sm font-semibold pt-2 pb-1 text-pink-300 lg:text-lg">
+              <p className="sm:text-sm  md:text-md font-semibold pt-2 pb-1 text-pink-300 lg:text-lg">
                 ${parseInt(product.price)}
               </p>
             </div>
 
             <div className="sm:col-start-2 sm:row-start-1 text-md font-bold text-pink-500 md:pt-0 md:row-start-1 md:col-start-2 lg:text-lg">
-              <p>Precio Unidad:</p>
+              <p className="sm:text-sm md:text-md lg:text-lg">Precio:</p>
             </div>
 
             <div className="total sm:col-start-1 sm:row-start-5 md:col-start-4 md:row-start-1 justify-center">
-              <p className="text-md font-bold text-pink-500 sm:text-left lg:text-lg">
+              <p className="sm:text-sm md:text-md font-bold text-pink-500 sm:text-left lg:text-lg">
                 Subtotal:
               </p>
             </div>
 
             <div className="sm:col-start-2 sm:row-start-5 md:col-start-4 md:row-start-2">
-              <p className="text-md font-bold text-pink-500 sm:text-center sm:justify-center lg:text-lg">
+              <p className="sm:text-sm md:text-md font-bold text-pink-500 sm:text-center sm:justify-center lg:text-lg">
                 ${parseInt(product.price) * productQuantities[product.productId]}
               </p>
             </div>
 
             <button
-              className="items-center sm:flex sm:justify-center sm:content-center w-6 h-6 font-medium rounded-md text-pink-400 bg-pink-200 transition-all duration-500 ease-in-out hover:text-pink-700 hover:bg-pink-300 sm:row-start-2 sm:col-start-4 md:w-10 md:h-10 md:col-start-5 md:row-start-1"
+              className="items-center sm:flex sm:justify-center sm:content-center sm:w-8 sm:h-8 font-medium rounded-md text-pink-400 bg-pink-200 transition-all duration-500 ease-in-out hover:text-pink-700 hover:bg-pink-300 sm:row-start-2 sm:col-start-4 md:w-10 md:h-10 md:col-start-5 md:row-start-1"
               onClick={() => handleIncrementQuantity(product.productId)}
             >
               +
             </button>
 
             <div className="sm:row-start-1 sm:col-start-3 md:col-start-3 md:row-start-1">
-              <p className="text-md font-bold text-pink-500 sm:text-center lg:text-lg">
+              <p className="sm:text-sm md:text-md font-bold text-pink-500 sm:text-center lg:text-lg">
                 Unidades:
               </p>
             </div>
 
-            <span className="text-md font-medium text-gray-400 md:text-lg sm:col-start-3 sm:row-start-2 sm:text-center md:col-start-3 md:row-start-2 lg:text-lg">
+            <span className="sm:text-sm text-md font-medium text-gray-400 md:text-md sm:col-start-3 sm:row-start-2 sm:text-center md:col-start-3 md:row-start-2 lg:text-lg">
               {productQuantities[product.productId]}
             </span>
 
             {productQuantities[product.productId] > 1 && (
               <button
-                className="sm:w-6 h-6 font-medium rounded-md text-pink-400 bg-pink-200 transition-all duration-500 ease-in-out hover:text-pink-700 hover:bg-pink-300 sm:col-start-4 sm:row-start-3 md:w-10 md:h-10 md:col-start-5 md:row-start-2"
+                className="sm:w-8 sm:h-8 font-medium rounded-md text-pink-400 bg-pink-200 transition-all duration-500 ease-in-out hover:text-pink-700 hover:bg-pink-300 sm:col-start-4 sm:row-start-3 md:w-10 md:h-10 md:col-start-5 md:row-start-2"
                 onClick={() => handleDecrementQuantity(product.productId)}
               >
                 -
@@ -155,7 +155,7 @@ const Cart = () => {
             )}
 
             <button
-              className="w-6 h-6 font-medium rounded-md bg-pink-600 text-white transition-all duration-500 ease-in-out hover:bg-white hover:text-pink-600 hover:border hover:border-pink-600 md:w-10 md:h-10 sm:col-start-4 sm:row-start-5 md:col-start-5 md:row-start-3"
+              className="sm:w-8 sm:h-8 font-medium rounded-md bg-pink-600 text-white transition-all duration-500 ease-in-out hover:bg-white hover:text-pink-600 hover:border hover:border-pink-600 md:w-10 md:h-10 sm:col-start-4 sm:row-start-5 md:col-start-5 md:row-start-3"
               onClick={() => handleRemoveProduct(product.productId)}
             >
               X
@@ -164,20 +164,20 @@ const Cart = () => {
         ))}
       </ul>
 
-      <div className="total flex justify-between pl-3 pr-3 items-center h-20 pb-2 border-b-2 shadow-sm border-gray-100 lg:h-60 lg:justify-evenly">
-        <p className="text-lg text-pink-600 font-semibold md:text-2xl">
+      <div className=" bg-pink-100 total flex justify-between pl-3 pr-3 items-center h-20 pb-2  shadow-sm border-gray-100 lg:h-60 lg:justify-evenly">
+        <p className=" text-lg text-pink-600 font-semibold md:text-2xl">
           Total del Carrito: ${totalCartPrice}
         </p>
         <button
-          className="w-22 h-10 text-xs font-medium rounded-md bg-red-700 text-white transition-all duration-500 ease-in-out hover:bg-transparent hover:border border-red-700 hover:text-black hover:scale-120-smooth md:w-28 md:h-30"
+          className="w-22 h-10 text-xs font-medium rounded-md bg-red-700 text-white transition-all duration-500 ease-in-out hover:bg-transparent hover:border border-red-700 hover:text-black hover:scale-120-smooth md:w-28 md:h-30 lg:w-38 lg:h-38"
           onClick={handleClearCart}
         >
           Vaciar Carrito
         </button>
       </div>
 
-      <div className="flex justify-around items-center content-center pt-5">
-        <button className="w-40 h-14 text-lg font-medium rounded-md bg-pink-500 text-white transition-all duration-500 ease-in-out hover:text-pink-500 hover:border hover:border-pink-500 hover:bg-pink-200 hover:scale-120-smooth">
+      <div className="bg-purple-200 flex justify-around items-center content-center pt-5 pb-5">
+        <button className=" w-40 h-14 text-lg font-medium rounded-md bg-pink-500 text-white transition-all duration-500 ease-in-out hover:text-pink-500 hover:border hover:border-pink-500 hover:bg-pink-200 hover:scale-120-smooth">
           Comprar
         </button>
       </div>
@@ -186,5 +186,22 @@ const Cart = () => {
 };
 
 export default Cart;
+
+
+
+
+
+
+
+
+
+
+{/* <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="22" height="22" viewBox="0 0 24 24" stroke-width="1.5" stroke="#db6699" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+  <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+  <path d="M17 17h-11v-14h-2" />
+  <path d="M6 5l14 1l-1 7h-13" />
+</svg> */}
 
 
