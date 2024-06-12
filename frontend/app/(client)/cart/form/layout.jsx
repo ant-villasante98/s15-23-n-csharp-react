c7d16a1d48bd797a710d2be9b1f6dd55fb0b342c
Johanna Hernandez <johannaviglianco@gmail.com>
2024-06-12 01:17:14 -0300

    Agrego responsive al form

--- frontend/app/(client)/cart/form/layout.jsx
+++ frontend/app/(client)/cart/form/layout.jsx
@@ -1,7 +1,7 @@
 const Layout = ({ children }) => {
     return (
-      <div className="bg-purple-300 flex min-h-screen flex-col">
-        <div className="w-full h-full mt-5 p-10">{children}</div>
+      <div className="bg-purple-300 flex min-h-screen flex-col ">
+        <div className="sm:pl-0 sm:pr-0 w-full h-full mt-5 p-10 flex flex-col justify-center items-center">{children}</div>
       </div>
     );
   };
--- frontend/app/(client)/cart/form/page.jsx
+++ frontend/app/(client)/cart/form/page.jsx
@@ -52,17 +52,17 @@ export const Form = () => {
   return (
     <>
 
-      <h2 className="pb-10 text-center text-2xl text-white font-medium drop-shadow-2xl  ">Por favor llena los siguientes datos para poder enviarte tu pedido a la brevedad</h2>
+      <h2 className="sm:text-xs sm:pb-4 sm:text-left md:text-base lg:text- pb-10 text-left text-2xl text-white font-medium drop-shadow-2xl">Por favor te pedimos que llenes los siguientes datos para poder enviarte tu pedido a la brevedad</h2>
       <form
         onSubmit={handleSubmit}
-        className="w-2/3 m-auto  bg-purple-400 p-10 flex flex-col gap-8 items-center rounded-xl"
+        className="pt-5 sm:w-full sm:ml-0 sm:mr-0 sm:pl-2 sm:pr-2 md:p-10 lg:w-2/3   bg-purple-400 flex flex-col gap-8 items-center rounded-xl"
       >
         <div>
-          <label htmlFor="name" className="block mb-2 text-white font-medium text-lg">Nombre: </label>
+          <label htmlFor="name" className="sm:text-sm block mb-2 text-white font-medium text-lg">Nombre: </label>
           <input
             id="name"
             autoComplete="on"
-            className="w-80 rounded-md h-8"
+            className="sm:w-50 sm:h-7 md:w-80 rounded-md h-8"
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
@@ -70,10 +70,10 @@ export const Form = () => {
           {errors.name && <p className="text-red-600 text-xs">{errors.name}</p>}
         </div>
         <div>
-          <label htmlFor="lastName" className="block mb-2  text-white font-medium text-lg">Apellido: </label>
+          <label htmlFor="lastName" className="sm:text-sm block mb-2  text-white font-medium text-lg">Apellido: </label>
           <input
             id="lastName"
-            className="w-80 rounded-md h-8"
+            className="sm:w-50 sm:h-7 md:w-80 rounded-md h-8"
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
@@ -81,11 +81,11 @@ export const Form = () => {
           {errors.lastName && <p className="text-red-600 text-xs">{errors.lastName}</p>}
         </div>
         <div>
-          <label htmlFor="email" className="block mb-2  text-white font-medium text-lg">Email: </label>
+          <label htmlFor="email" className="sm:text-sm block mb-2  text-white font-medium text-lg">Email: </label>
           <input
             id="email"
             autoComplete="on"
-            className="w-80 rounded-md h-8"
+            className="sm:w-50 sm:h-7 md:w-80 rounded-md h-8"
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -93,20 +93,20 @@ export const Form = () => {
           {errors.email && <p className="text-red-600 text-xs">{errors.email}</p>}
         </div>
         <div>
-          <label htmlFor="adress" className="block mb-2  text-white font-medium text-lg">Direccion: </label>
+          <label htmlFor="adress" className="sm:text-sm block mb-2  text-white font-medium text-lg">Direccion: </label>
           <input
             id="adress"
-            className=" w-80 rounded-md h-8"
+            className="sm:w-50 sm:h-7 md:w-80 rounded-md h-8"
             type="text"
             value={adress}
             onChange={(e) => setAdress(e.target.value)}
           />
         </div>
         <div>
-          <label htmlFor="postalCode" className="block mb-2  text-white font-medium text-lg">Código Postal: </label>
+          <label htmlFor="postalCode" className="sm:text-sm block mb-2  text-white font-medium text-lg">Código Postal: </label>
           <input
             id="postalCode"
-            className="w-80 rounded-md h-8"
+            className="sm:w-50 sm:h-7 md:w-80 rounded-md h-8"
             type="text"
             value={postalCode}
             onChange={(e) => setPostalCode(e.target.value)}
@@ -114,10 +114,10 @@ export const Form = () => {
           {errors.postalCode && <p className="text-red-600 text-xs">{errors.postalCode}</p>}
         </div>
         <div>
-          <label htmlFor="cellphone" className="block mb-2 mr-2  text-white font-medium text-lg">Teléfono: </label>
+          <label htmlFor="cellphone" className="sm:text-sm block mb-2 mr-2  text-white font-medium text-lg">Teléfono: </label>
           <input
             id="cellphone"
-            className=" w-80 rounded-md h-8"
+            className="sm:w-50 sm:h-7 md:w-80 rounded-md h-8"
             type="text"
             value={cellphone}
             onChange={(e) => setCellphone(e.target.value)}
